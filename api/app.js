2d24a48051831929277874e59a9f4e311a47a8bb
RizWaaN3024 <rizwaanans786@gmail.com>
2024-07-01 03:23:08 +0530

    Initial Setip

--- api/app.js
+++ api/app.js
@@ -3,6 +3,10 @@ import express from "express"
 
 const app = express()
 
+app.use("/api/test", (req, res) => {
+    res.send("Hello")
+})
+
 app.listen(8800, () => {
     console.log("server is running")
 })
