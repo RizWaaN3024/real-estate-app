63428d6e9db9d18ee5d52b7316e81b686a800852
RizWaaN3024 <rizwaanans786@gmail.com>
2024-08-01 14:06:15 +0530

    Updated api routes

---
+++ api/routes/post.route.js
@@ -0,0 +1,10 @@
+import express from "express";
+
+const router = express.Router();
+
+router.get("/test", (req, res) => {
+    console.log("router works")
+    res.send("Router Working")
+})
+
+export default router;
