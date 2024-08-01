af57fad9367b8b40fe1c4f33c69c0fb715095b72
RizWaaN3024 <rizwaanans786@gmail.com>
2024-08-01 14:18:56 +0530

    Auth route initia setup

---
+++ api/routes/auth.route.js
@@ -0,0 +1,15 @@
+import express from "express";
+
+const router = express.Router();
+
+router.post("/register", (req, res) => {
+
+})
+
+router.post("/login", (req, res) => {
+
+})
+
+router.post("/logout", (req, res) => {
+    
+})
