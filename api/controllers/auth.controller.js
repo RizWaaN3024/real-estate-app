3f5486a99a8eeaf747f433aa2dbbc61487c5ddb7
RizWaaN3024 <rizwaanans786@gmail.com>
2024-08-03 01:24:45 +0530

    Created User

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,21 +1,35 @@
 import bcrypt from "bcrypt";
+import prisma from "../lib/prisma.js";
 
 export const register = async (req, res) => {
-    // destructuring the req data
-    const { username, email, password } = req.body;
-    
+  // destructuring the req data
+  const { username, email, password } = req.body;
+
+  try {
     // Hash the password
     const hashPassword = await bcrypt.hash(password, 10);
-    console.log(hashPassword)
-    // After hashing create new user 
+    console.log(hashPassword);
+    // After hashing create new user
+    const newUser = await prisma.user.create({
+      data: {
+        username,
+        email,
+        password: hashPassword,
+      },
+    });
 
-    
-}
+    console.log(newUser);
+    res.status(201).json({ message: "User created successfully" });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ message: "Failed to create user" });
+  }
+};
 
 export const login = (req, res) => {
-    // DB operations
-}
+  // DB operations
+};
 
 export const logout = (req, res) => {
-    // DB operations
-}
+  // DB operations
+};
