78d7724283c897865f1d8399816f0f3ae8969bee
RizWaaN3024 <rizwaanans786@gmail.com>
2024-08-22 23:19:16 +0530

    Test

--- api/routes/auth.route.js
+++ api/routes/auth.route.js
@@ -3,10 +3,13 @@ import { login, logout, register } from "../controllers/auth.controller.js";
 
 const router = express.Router();
 
+
 router.post("/register", register)
 
 router.post("/login", login)
 
 router.post("/logout", logout)
+
+
 
 export default router;
